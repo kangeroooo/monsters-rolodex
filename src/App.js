f5b096c69d39337df9c2cd66ce34907b0ef2c6f1
Wei Luo <alexcluoo@gmail.com>
2021-05-19 18:24:50 -0500

    add title and background

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ class App extends Component {
     const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()));
     return (
       <div className="App">
+        <h1> Monsters Rolodox </h1>
         <SearchBox
           placeholder='search monsters'
           handleChange={e => this.setState({ searchField: e.target.value })}
